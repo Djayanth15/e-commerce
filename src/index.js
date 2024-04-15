93787fe4dd9f58665951eb4aa8d105f21b033060
Djayanth15 <dannana.jayanth@gmail.com>
2024-04-14 23:59:11 -0400

    utils code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
 dotenv.config({ path: "./env" });
+import { app } from "./app.js";
 
-connectDB();
+(async () => {
+  try {
+    await connectDB();
+    app.on("error", (error) => {
+      console.log("Connection error(idx) :", error);
+      throw error;
+    });
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`server is running on port: ${port}`);
+    });
+  } catch (error) {
+    console.log("error :: idx/src :", error);
+  }
+})();
